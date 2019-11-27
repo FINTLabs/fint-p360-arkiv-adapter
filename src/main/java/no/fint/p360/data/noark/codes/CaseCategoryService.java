385d43918482d06a2df657c782752cb5eaba5a16
Asgeir Nilsen <asgeir@fintlabs.no>
2019-11-27 13:14:29 +0100

    Syntax error.

--- src/main/java/no/fint/p360/data/noark/codes/CaseCategoryService.java
+++ src/main/java/no/fint/p360/data/noark/codes/CaseCategoryService.java
@@ -14,7 +14,7 @@ public class CaseCategoryService {
     @Autowired
     private P360SupportService supportService;
 
-    @Value("${fint.p360.tables.document-status:code table: Case category")
+    @Value("${fint.p360.tables.document-status:code table: Case category}")
     private String documentStatusTable;
 
     public Stream<String> getCaseCategoryTable() {
