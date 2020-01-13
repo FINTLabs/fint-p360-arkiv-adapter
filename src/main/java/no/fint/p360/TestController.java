c1397541f5621a8246e5d147c5f0978df5041a3c
Asgeir Storesund Nilsen <asgeir@fintlabs.no>
2020-01-13 17:08:44 +0100

    Added AccessGroupService and UserService (#27)
    * Added UserService.
    
    * futz
    
    * Add AccessGroupService
    
    * Enable UserServicec WSDL compilation
    
    * updated user service wsdl
    
    * Revert "updated user service wsdl"
    
    This reverts commit 1a1af6f25a8715807f66a539aff40eba01281ddb.
    
    * Use bindings instead.
    
    Fix name conflict with bindings instead of modifying WSDL & schema.
    
    Co-authored-by: Frode Sjovatsen <fsjovatsen@users.noreply.github.com>

--- src/main/java/no/fint/p360/TestController.java
+++ src/main/java/no/fint/p360/TestController.java
@@ -2,6 +2,9 @@ package no.fint.p360;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import no.fint.arkiv.p360.accessgroup.GetAccessGroupsQuery;
+import no.fint.arkiv.p360.accessgroup.ObjectFactory;
+import no.fint.p360.data.p360.P360AccessGroupService;
 import no.fint.p360.data.p360.P360SupportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -14,9 +17,19 @@ public class TestController {
 
     @Autowired
     private P360SupportService supportService;
+    
+    @Autowired
+    private P360AccessGroupService accessGroupService;
 
     @GetMapping(value = "codelist", produces = MediaType.APPLICATION_JSON_VALUE)
     public String getCodelist(@RequestParam String id) throws JsonProcessingException {
         return new ObjectMapper().writeValueAsString(supportService.getCodeTable(id));
+    }
+    
+    @GetMapping(value = "accessgroup", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getAccessGroup() throws JsonProcessingException {
+        final ObjectFactory factory = new ObjectFactory();
+        GetAccessGroupsQuery parameter = factory.createGetAccessGroupsQuery();
+        return new ObjectMapper().writeValueAsString(accessGroupService.getAccessGroups(parameter));
     }
 }
