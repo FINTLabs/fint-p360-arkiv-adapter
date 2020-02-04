851b3d58dd1d87e0ad014b6813b795df696d6573
Asgeir Nilsen <asgeir@fintlabs.no>
2020-02-04 16:38:22 +0100

    New TitleService for case titles
    Also refactored TilskuddFartoyFactory, pulling code up to NoarkFactory.

--- src/main/java/no/fint/p360/data/kulturminne/TilskuddFartoyFactory.java
+++ src/main/java/no/fint/p360/data/kulturminne/TilskuddFartoyFactory.java
@@ -1,42 +1,28 @@
 package no.fint.p360.data.kulturminne;
 
-import com.google.common.base.Strings;
 import lombok.extern.slf4j.Slf4j;
-import no.fint.arkiv.p360.caze.*;
+import no.fint.arkiv.p360.caze.CaseResult;
+import no.fint.arkiv.p360.caze.CreateCaseParameter;
+import no.fint.arkiv.p360.caze.ObjectFactory;
 import no.fint.arkiv.p360.document.CreateDocumentParameter;
-import no.fint.model.administrasjon.arkiv.Saksstatus;
-import no.fint.model.administrasjon.organisasjon.Organisasjonselement;
-import no.fint.model.administrasjon.personal.Personalressurs;
 import no.fint.model.felles.kompleksedatatyper.Identifikator;
 import no.fint.model.kultur.kulturminnevern.TilskuddFartoy;
 import no.fint.model.resource.Link;
 import no.fint.model.resource.administrasjon.arkiv.JournalpostResource;
-import no.fint.model.resource.administrasjon.arkiv.MerknadResource;
-import no.fint.model.resource.administrasjon.arkiv.PartsinformasjonResource;
-import no.fint.model.resource.administrasjon.arkiv.SaksstatusResource;
 import no.fint.model.resource.kultur.kulturminnevern.TilskuddFartoyResource;
 import no.fint.p360.data.exception.GetDocumentException;
 import no.fint.p360.data.exception.IllegalCaseNumberFormat;
-import no.fint.p360.data.exception.NoSuchTitleDimension;
-import no.fint.p360.data.exception.UnableToParseTitle;
 import no.fint.p360.data.noark.common.NoarkFactory;
 import no.fint.p360.data.noark.journalpost.JournalpostFactory;
 import no.fint.p360.data.noark.korrespondansepart.KorrespondansepartFactory;
-import no.fint.p360.data.utilities.FintUtils;
 import no.fint.p360.data.utilities.NOARKUtils;
 import no.fint.p360.data.utilities.P360Utils;
-import no.fint.p360.data.utilities.TitleParser;
-import no.fint.p360.repository.KodeverkRepository;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
-
-import static no.fint.p360.data.utilities.FintUtils.optionalValue;
-import static no.fint.p360.data.utilities.P360Utils.applyParameterFromLink;
 
 @Slf4j
 @Service
@@ -65,20 +51,13 @@ public class TilskuddFartoyFactory {
     public TilskuddFartoyResource toFintResource(CaseResult caseResult) throws GetDocumentException, IllegalCaseNumberFormat {
 
         TilskuddFartoyResource tilskuddFartoy = new TilskuddFartoyResource();
+        tilskuddFartoy.setSoknadsnummer(new Identifikator());
+        tilskuddFartoy.setMappeId(new Identifikator());
+        tilskuddFartoy.setSystemId(new Identifikator());
         String caseNumber = caseResult.getCaseNumber().getValue();
 
         String caseYear = NOARKUtils.getCaseYear(caseNumber);
         String sequenceNumber = NOARKUtils.getCaseSequenceNumber(caseNumber);
-
-        try {
-            TitleParser.Title title = TitleParser.parseTitle(caseResult.getTitle().getValue());
-            tilskuddFartoy.setFartoyNavn(Strings.nullToEmpty(title.getDimension(TitleParser.FARTOY_NAVN)));
-            tilskuddFartoy.setKallesignal(Strings.nullToEmpty(title.getDimension(TitleParser.FARTOY_KALLESIGNAL)));
-            tilskuddFartoy.setKulturminneId(Strings.nullToEmpty(title.getDimension(TitleParser.KULTURMINNE_ID)));
-            tilskuddFartoy.setSoknadsnummer(FintUtils.createIdentifikator(caseResult.getExternalId().getValue().getId().getValue()));
-        } catch (UnableToParseTitle | NoSuchTitleDimension e) {
-            log.error("{}", e.getMessage(), e);
-        }
 
         noarkFactory.getSaksmappe(caseResult, tilskuddFartoy);
 
@@ -103,123 +82,13 @@ public class TilskuddFartoyFactory {
 
         tilskuddFartoyDefaults.applyDefaultsToCreateCase(tilskuddFartoy, createCaseParameter);
 
-        createCaseParameter.setTitle(objectFactory.createCaseParameterBaseTitle(TitleParser.getTitleString(tilskuddFartoy)));
         createCaseParameter.setExternalId(P360Utils.getExternalIdParameter(tilskuddFartoy.getSoknadsnummer()));
 
-        applyParameterFromLink(
-                tilskuddFartoy.getAdministrativEnhet(),
-                s -> objectFactory.createCaseParameterBaseResponsibleEnterpriseRecno(Integer.valueOf(s)),
-                createCaseParameter::setResponsibleEnterpriseRecno
-        );
-
-        applyParameterFromLink(
-                tilskuddFartoy.getArkivdel(),
-                objectFactory::createCaseParameterBaseSubArchive,
-                createCaseParameter::setSubArchive
-        );
-
-        applyParameterFromLink(
-                tilskuddFartoy.getSaksstatus(),
-                objectFactory::createCaseParameterBaseStatus,
-                createCaseParameter::setStatus
-        );
-
-        if (tilskuddFartoy.getSkjerming() != null) {
-            applyParameterFromLink(
-                    tilskuddFartoy.getSkjerming().getTilgangsrestriksjon(),
-                    objectFactory::createCaseParameterBaseAccessCode,
-                    createCaseParameter::setAccessCode);
-
-            applyParameterFromLink(
-                    tilskuddFartoy.getSkjerming().getSkjermingshjemmel(),
-                    objectFactory::createCaseParameterBaseParagraph,
-                    createCaseParameter::setParagraph);
-
-            // TODO createCaseParameter.setAccessGroup();
-        }
-
-        // TODO createCaseParameter.setCategory(objectFactory.createCaseParameterBaseCategory("recno:99999"));
-        // TODO Missing parameters
-        //createCaseParameter.setRemarks();
-        //createCaseParameter.setStartDate();
-        //createCaseParameter.setUnofficialTitle();
-
-        ArrayOfCaseContactParameter arrayOfCaseContactParameter = objectFactory.createArrayOfCaseContactParameter();
-        tilskuddFartoy
-                .getPart()
-                .stream()
-                .map(this::createCaseContactParameter)
-                .forEach(arrayOfCaseContactParameter.getCaseContactParameter()::add);
-        createCaseParameter.setContacts(objectFactory.createCaseParameterBaseContacts(arrayOfCaseContactParameter));
-
-        ArrayOfRemark arrayOfRemark = objectFactory.createArrayOfRemark();
-        if (tilskuddFartoy.getMerknad() != null) {
-            tilskuddFartoy
-                    .getMerknad()
-                    .stream()
-                    .map(this::createCaseRemarkParameter)
-                    .forEach(arrayOfRemark.getRemark()::add);
-        }
-        createCaseParameter.setRemarks(objectFactory.createCaseParameterBaseRemarks(arrayOfRemark));
-
-        // TODO Responsible person
-        /*
-        createCaseParameter.setResponsiblePersonIdNumber(
-                objectFactory.createCaseParameterBaseResponsiblePersonIdNumber(
-                        tilskuddFartoy.getSaksansvarlig().get(0).getHref()
-                )
-        );
-        */
+        noarkFactory.applyCaseParameters(tilskuddFartoy, createCaseParameter);
 
         return createCaseParameter;
     }
 
-    private Remark createCaseRemarkParameter(MerknadResource merknadResource) {
-        Remark remark = objectFactory.createRemark();
-        remark.setContent(objectFactory.createRemarkContent(merknadResource.getMerknadstekst()));
-
-        merknadResource
-                .getMerknadstype()
-                .stream()
-                .map(Link::getHref)
-                .filter(StringUtils::isNotBlank)
-                .map(s -> StringUtils.substringAfterLast(s, "/"))
-                .map(s -> StringUtils.prependIfMissing(s, "recno:"))
-                .map(objectFactory::createRemarkRemarkType)
-                .findFirst()
-                .ifPresent(remark::setRemarkType);
-
-        return remark;
-    }
-
-
-    public CaseContactParameter createCaseContactParameter(PartsinformasjonResource partsinformasjon) {
-        CaseContactParameter caseContactParameter = objectFactory.createCaseContactParameter();
-
-        partsinformasjon
-                .getPart()
-                .stream()
-                .map(Link::getHref)
-                .filter(StringUtils::isNotBlank)
-                .map(s -> StringUtils.substringAfterLast(s, "/"))
-                .map(s -> StringUtils.prependIfMissing(s, "recno:"))
-                .map(objectFactory::createCaseContactParameterReferenceNumber)
-                .findFirst()
-                .ifPresent(caseContactParameter::setReferenceNumber);
-
-        partsinformasjon
-                .getPartRolle()
-                .stream()
-                .map(Link::getHref)
-                .filter(StringUtils::isNotBlank)
-                .map(s -> StringUtils.substringAfterLast(s, "/"))
-                .map(s -> StringUtils.prependIfMissing(s, "recno:"))
-                .map(objectFactory::createCaseContactParameterRole)
-                .findFirst()
-                .ifPresent(caseContactParameter::setRole);
-
-        return caseContactParameter;
-    }
 
     public CreateDocumentParameter convertToCreateDocument(JournalpostResource journalpostResource, String caseNumber) {
         CreateDocumentParameter createDocumentParameter = journalpostFactory.toP360(journalpostResource, caseNumber);
--- src/main/java/no/fint/p360/data/noark/common/NoarkFactory.java
+++ src/main/java/no/fint/p360/data/noark/common/NoarkFactory.java
@@ -18,10 +18,12 @@ import no.fint.p360.data.p360.P360DocumentService;
 import no.fint.p360.data.utilities.FintUtils;
 import no.fint.p360.data.utilities.NOARKUtils;
 import no.fint.p360.repository.KodeverkRepository;
+import no.fint.p360.service.TitleService;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.xml.bind.JAXBElement;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +31,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static no.fint.p360.data.utilities.FintUtils.optionalValue;
+import static no.fint.p360.data.utilities.P360Utils.applyParameterFromLink;
 
 @Service
 public class NoarkFactory {
@@ -44,6 +47,16 @@ public class NoarkFactory {
 
     @Autowired
     private PartFactory partFactory;
+
+    @Autowired
+    private TitleService titleService;
+
+    private ObjectFactory objectFactory;
+
+    @PostConstruct
+    private void init() {
+        objectFactory = new ObjectFactory();
+    }
 
     public void getSaksmappe(CaseResult caseResult, SaksmappeResource saksmappeResource) throws GetDocumentException, IllegalCaseNumberFormat {
         String caseNumber = caseResult.getCaseNumber().getValue();
@@ -130,6 +143,123 @@ public class NoarkFactory {
                 .map(Identifikator::getIdentifikatorverdi)
                 .map(Link.apply(KlasseResource.class, "systemid"))
                 .forEach(saksmappeResource::addKlasse);
+
+        titleService.parseTitle(saksmappeResource, saksmappeResource.getTittel());
+    }
+
+    public void applyCaseParameters(SaksmappeResource saksmappeResource, CreateCaseParameter createCaseParameter) {
+        createCaseParameter.setTitle(objectFactory.createCaseParameterBaseTitle(titleService.getTitle(saksmappeResource)));
+        applyParameterFromLink(
+                saksmappeResource.getAdministrativEnhet(),
+                s -> objectFactory.createCaseParameterBaseResponsibleEnterpriseRecno(Integer.valueOf(s)),
+                createCaseParameter::setResponsibleEnterpriseRecno
+        );
+
+        applyParameterFromLink(
+                saksmappeResource.getArkivdel(),
+                objectFactory::createCaseParameterBaseSubArchive,
+                createCaseParameter::setSubArchive
+        );
+
+        applyParameterFromLink(
+                saksmappeResource.getSaksstatus(),
+                objectFactory::createCaseParameterBaseStatus,
+                createCaseParameter::setStatus
+        );
+
+        if (saksmappeResource.getSkjerming() != null) {
+            applyParameterFromLink(
+                    saksmappeResource.getSkjerming().getTilgangsrestriksjon(),
+                    objectFactory::createCaseParameterBaseAccessCode,
+                    createCaseParameter::setAccessCode);
+
+            applyParameterFromLink(
+                    saksmappeResource.getSkjerming().getSkjermingshjemmel(),
+                    objectFactory::createCaseParameterBaseParagraph,
+                    createCaseParameter::setParagraph);
+
+            // TODO createCaseParameter.setAccessGroup();
+        }
+
+        // TODO createCaseParameter.setCategory(objectFactory.createCaseParameterBaseCategory("recno:99999"));
+        // TODO Missing parameters
+        //createCaseParameter.setRemarks();
+        //createCaseParameter.setStartDate();
+        //createCaseParameter.setUnofficialTitle();
+
+        ArrayOfCaseContactParameter arrayOfCaseContactParameter = objectFactory.createArrayOfCaseContactParameter();
+        saksmappeResource
+                .getPart()
+                .stream()
+                .map(this::createCaseContactParameter)
+                .forEach(arrayOfCaseContactParameter.getCaseContactParameter()::add);
+        createCaseParameter.setContacts(objectFactory.createCaseParameterBaseContacts(arrayOfCaseContactParameter));
+
+        ArrayOfRemark arrayOfRemark = objectFactory.createArrayOfRemark();
+        if (saksmappeResource.getMerknad() != null) {
+            saksmappeResource
+                    .getMerknad()
+                    .stream()
+                    .map(this::createCaseRemarkParameter)
+                    .forEach(arrayOfRemark.getRemark()::add);
+        }
+        createCaseParameter.setRemarks(objectFactory.createCaseParameterBaseRemarks(arrayOfRemark));
+
+        // TODO Responsible person
+        /*
+        createCaseParameter.setResponsiblePersonIdNumber(
+                objectFactory.createCaseParameterBaseResponsiblePersonIdNumber(
+                        tilskuddFartoy.getSaksansvarlig().get(0).getHref()
+                )
+        );
+        */
+    }
+
+    private Remark createCaseRemarkParameter(MerknadResource merknadResource) {
+        Remark remark = objectFactory.createRemark();
+        remark.setContent(objectFactory.createRemarkContent(merknadResource.getMerknadstekst()));
+
+        merknadResource
+                .getMerknadstype()
+                .stream()
+                .map(Link::getHref)
+                .filter(StringUtils::isNotBlank)
+                .map(s -> StringUtils.substringAfterLast(s, "/"))
+                .map(s -> StringUtils.prependIfMissing(s, "recno:"))
+                .map(objectFactory::createRemarkRemarkType)
+                .findFirst()
+                .ifPresent(remark::setRemarkType);
+
+        return remark;
+    }
+
+
+    public CaseContactParameter createCaseContactParameter(PartsinformasjonResource partsinformasjon) {
+        CaseContactParameter caseContactParameter = objectFactory.createCaseContactParameter();
+
+        partsinformasjon
+                .getPart()
+                .stream()
+                .map(Link::getHref)
+                .filter(StringUtils::isNotBlank)
+                .map(s -> StringUtils.substringAfterLast(s, "/"))
+                .map(s -> StringUtils.prependIfMissing(s, "recno:"))
+                .map(objectFactory::createCaseContactParameterReferenceNumber)
+                .findFirst()
+                .ifPresent(caseContactParameter::setReferenceNumber);
+
+        partsinformasjon
+                .getPartRolle()
+                .stream()
+                .map(Link::getHref)
+                .filter(StringUtils::isNotBlank)
+                .map(s -> StringUtils.substringAfterLast(s, "/"))
+                .map(s -> StringUtils.prependIfMissing(s, "recno:"))
+                .map(objectFactory::createCaseContactParameterRole)
+                .findFirst()
+                .ifPresent(caseContactParameter::setRole);
+
+        return caseContactParameter;
     }
 
     private PartsinformasjonResource createPartsinformasjon(CaseContactResult caseContactResult) {
